1a314b94702acf452a051e4482ebeb18078386c7
surekhad96 <surekhasugan96@gmail.com>
2017-06-07 15:39:47 +0530

    Create greatest_of_three.c

---
+++ greatest_of_three.c
@@ -0,0 +1,21 @@
+# include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c))
+{
+printf("%d is greatest\n");
+}
+else if((b>a)&&(b>c))
+{
+printf("%d is greatest\n");
+}
+else
+{
+printf("%d is greatest\n");
+}
+getch();
+return 0;
+}
